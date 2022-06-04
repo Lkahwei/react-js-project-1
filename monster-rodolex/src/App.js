e952e42375fc0c8aa40d5c26580dcc4343450e48
Brixton <lkw06182000@hotmail.com>
2022-06-04 15:50:42 +0800

    done lesson 1

--- monster-rodolex/src/App.js
+++ monster-rodolex/src/App.js
@@ -1,25 +1,47 @@
+/*
+  In order to create class component
+  1. import React, { Component } from 'react';
+  2. 
+*/
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    //super() is to inherit the constructor method from the Component
+    //After using super(); method, we can then use this.state
+
+    super();
+
+    this.state = {
+      monster: [
+        {
+          name: 'Franken'
+        },
+        {
+          name: 'Dracula'
+        },
+        {
+          name: 'Zombie'
+        },
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {/* {} will tell react that the code inside this curly brackets is going to be javascript code */}
+            Hello {this.state.string}
+          </p>
+          <button onClick={() => this.setState({string: 'Hello Brixton'})}>Change Text</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
